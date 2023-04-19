f4a09550b87794d035910e6ebeccd947b4463d62
sujang958 <enter7377@naver.com>
2023-04-19 23:13:18 +0900

    feat: implement a date picker

---
+++ src/components/DaySelectList.svelte
@@ -0,0 +1,42 @@
+<button
+  type="button"
+  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+>
+  <p class="text-xl font-bold">일</p>
+</button>
+<button
+  type="button"
+  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+>
+  <p class="text-xl font-bold">월</p>
+</button>
+<button
+  type="button"
+  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+>
+  <p class="text-xl font-bold">화</p>
+</button>
+<button
+  type="button"
+  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+>
+  <p class="text-xl font-bold">수</p>
+</button>
+<button
+  type="button"
+  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+>
+  <p class="text-xl font-bold">목</p>
+</button>
+<button
+  type="button"
+  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+>
+  <p class="text-xl font-bold">금</p>
+</button>
+<button
+  type="button"
+  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+>
+  <p class="text-xl font-bold">토</p>
+</button>
