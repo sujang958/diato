31419cdfe16178a6257598160c028bc456a84f38
sujang958 <enter7377@naver.com>
2023-04-19 23:18:11 +0900

    feat: implement displaying the selected day

--- src/components/DaySelectList.svelte
+++ src/components/DaySelectList.svelte
@@ -1,42 +1,46 @@
+<script lang="ts">
+  import { focusedDate } from "../utils/atoms"
+</script>
+
 <button
   type="button"
-  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+  class={`${$focusedDate.getDay() == 0 ? "bg-black/10" : ""} h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700`}
 >
   <p class="text-xl font-bold">일</p>
 </button>
 <button
   type="button"
-  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+  class={`${$focusedDate.getDay() == 1 ? "bg-black/10" : ""} h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700`}
 >
   <p class="text-xl font-bold">월</p>
 </button>
 <button
   type="button"
-  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+  class={`${$focusedDate.getDay() == 2 ? "bg-black/10" : ""} h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700`}
 >
   <p class="text-xl font-bold">화</p>
 </button>
 <button
   type="button"
-  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+  class={`${$focusedDate.getDay() == 3 ? "bg-black/10" : ""} h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700`}
 >
   <p class="text-xl font-bold">수</p>
 </button>
 <button
   type="button"
-  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+  class={`${$focusedDate.getDay() == 4 ? "bg-black/10" : ""} h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700`}
 >
   <p class="text-xl font-bold">목</p>
 </button>
 <button
   type="button"
-  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+  class={`${$focusedDate.getDay() == 5 ? "bg-black/10" : ""} h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700`}
 >
   <p class="text-xl font-bold">금</p>
 </button>
 <button
   type="button"
-  class="h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700"
+  class={`${$focusedDate.getDay() == 6 ? "bg-black/10" : ""} h-8 w-8 grid place-items-center rounded-lg transition duration-200 hover:bg-black/5 text-neutral-700`}
 >
   <p class="text-xl font-bold">토</p>
 </button>
