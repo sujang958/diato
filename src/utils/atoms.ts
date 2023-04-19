21d39612782fd666819fffb58650f5ecf4b34a46
sujang958 <enter7377@naver.com>
2023-04-19 23:13:01 +0900

    feat: implement a date picker

--- src/utils/atoms.ts
+++ src/utils/atoms.ts
@@ -3,3 +3,5 @@ import { atom } from "nanostores"
 export const idToken = atom<null | string>(null)
 
 export const token = atom<null | string>(localStorage.getItem("token"))
+
+export const focusedDate = atom<Date>(new Date())
