f9f53d8ea2f99a7baa1688cf170156901cb9dad5
sujang958 <enter7377@naver.com>
2023-04-20 21:49:38 +0900

    fix: fix localStroage not defined error?

--- src/utils/atoms.ts
+++ src/utils/atoms.ts
@@ -2,6 +2,6 @@ import { atom } from "nanostores"
 
 export const idToken = atom<null | string>(null)
 
-export const token = atom<null | string>(localStorage?.getItem("token") ?? null)
+export const token = atom<null | string>(null)
 
 export const focusedDate = atom<Date>(new Date())
