2f5028ba1b11b5173588aff5ce83d519be7d8082
sujang958 <enter7377@naver.com>
2023-05-22 23:01:01 +0900

    fix: remove unused things

--- src/utils/atoms.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "nanostores"
-
-export const idToken = atom<null | string>(null)
-
-export const token = atom<null | string>(null)
-
-export const focusedDate = atom<Date>(new Date())
