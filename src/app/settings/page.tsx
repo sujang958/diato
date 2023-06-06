b323a281f7219e8d1a3c3460ede0fe52e77be5a3
sujang.958 <enter7377@naver.com>
2023-06-06 22:05:59 +0900

    feat: implement going back to the page"

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,32 +1,11 @@
-"use client"
-
-import { useAtom } from "jotai"
+import GoBackButton from "@/components/GoBackButton"
 import { NextPage } from "next"
-import  { useRouter } from "next/router"
-import { useEffect } from "react"
 
 const SettingsPgae: NextPage = () => {
-
-
   return (
     <div className="flex flex-col">
       <header className="fixed top-12">
-        <button type="button">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
-        </button>
+        <GoBackButton />
       </header>
       <div className="py-8"></div>
     </div>
