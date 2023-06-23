83c7da9ec29f92edcaebdc334b92e56cc3eaf04e
sujang.958 <enter7377@naver.com>
2023-06-23 22:35:36 +0900

    feat: add a loading animation

--- src/app/[day]/loading.tsx
+++ src/app/[day]/loading.tsx
@@ -1,3 +1,15 @@
+"use client"
+
+import Lottie from "lottie-react"
+
 export default function Loading() {
-  return "Loading..." // TODO: add something cool
+  return (
+    <div className="h-full w-full grid place-items-center">
+      <Lottie
+        animationData={require("../../../public/lotties/loading.json")}
+        loop={true}
+        className="w-8 h-8"
+      />
+    </div>
+  )
 }
